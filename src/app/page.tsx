c45d665e570506bb504cb357910b8e7bef478240
Prabhu K H <25prabhu10@gmail.com>
2025-01-13 00:31:57 -0500

    feat: add loading and not found components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
-export default function Home() {
+export default async function Home() {
+  // add 5 sec delay
+  await new Promise((resolve) => setTimeout(resolve, 5000))
+  console.log("Hello, Next in line")
   return <h1>Hello, Next in line</h1>
 }
